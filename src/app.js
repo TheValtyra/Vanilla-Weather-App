1ac16c9022f11f5d91eda0787ae6a29ff1e4d6d7
TheValtyra <115049631+TheValtyra@users.noreply.github.com>
2022-11-17 20:48:29 -0500

    linking more api to the page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "13cd370ea29aa99f56340dbf634b07d5";
