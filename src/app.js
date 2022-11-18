d28ca9d156ecce2217033298b9f4eb6084fbb1b5
TheValtyra <115049631+TheValtyra@users.noreply.github.com>
2022-11-17 22:05:15 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "13cd370ea29aa99f56340dbf634b07d5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
